b68a110c1a3ed87c990573c917c0c4aef00d7391
Catalin Casuneanu <catalin.casuneanu@jay-software.com>
2024-05-06 02:12:23 +0300

    chore: more seo stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,24 @@ import { defineConfig } from 'astro/config';
 import vercelStatic from "@astrojs/vercel/static";
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercelStatic({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
     imagesConfig: {
       sizes: [320, 640, 1280],
-      domains: ["jay-software.com"],
+      domains: ["jay-software.com"]
     },
     imageService: true,
     isr: {
-      expiration: 60 * 60 * 24,
-    },
+      expiration: 60 * 60 * 24
+    }
   }),
-  integrations: [tailwind()]
+  site: "https://jay-software.com",
+  integrations: [tailwind(), sitemap()]
 });
