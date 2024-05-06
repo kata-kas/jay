1e10188ff878f90ecef5e8df224465b264fb6a8d
Catalin Casuneanu <catalin.casuneanu@jay-software.com>
2024-05-06 18:44:41 +0300

    feat: services section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				marquee: 'marquee 25s linear infinite',
+				marquee2: 'marquee2 25s linear infinite',
+			},
+			keyframes: {
+				marquee: {
+					'0%': { transform: 'translateX(0%)' },
+					'100%': { transform: 'translateX(-100%)' },
+				},
+				marquee2: {
+					'0%': { transform: 'translateX(100%)' },
+					'100%': { transform: 'translateX(0%)' },
+				},
+			},
+		},
 	},
 	plugins: [],
 }
